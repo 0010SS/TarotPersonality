4730eb8083d9543e0e771b1482f0ced0d45238c1
Hardy Wen <103312042+0010SS@users.noreply.github.com>
2023-01-10 16:25:36 +0800

    First Version in Blazor WASM

---
+++ Data/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TarotPersonality.Data
+{
+    public class Card  // the class for the each card
+    {
+        public int id { get; set; }
+        public string field { get; set; }
+        public string deg { get; set; }
+        public bool flipped { get; set; }
+        public string pattern { get; set; }
+        public string graph { get; set; }
+    }
+}
+
